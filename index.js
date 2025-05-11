596b0f877bab310ed16559d8d7c28a70227a3f03
Eiko919 <arthursugiyama@icloud.com>
2025-05-11 00:38:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -43,9 +43,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-require('dotenv').config();
-const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
